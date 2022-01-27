4a86a39d106a1205077bb124c66051ac571108f2
juncat <blue3197@gmail.com>
2022-01-27 23:48:59 +0900

    20220127 글 생성 UI 만들기

--- Nodejs/main.js
+++ Nodejs/main.js
@@ -13,6 +13,8 @@ function templateHTML(title, list, body) {
       <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <button type="button" onclick="location.href='/create'">create</button>
+        <a href="/create">create</a>
         ${body}
       </body>
       </html>
@@ -35,6 +37,8 @@ var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
+  console.log(pathname);
+
   if (pathname === '/') {
     if (queryData.id === undefined) {
       fs.readdir('./data', function (err, filelist) {
@@ -65,6 +69,28 @@ var app = http.createServer(function (request, response) {
         );
       });
     }
+  } else if (pathname === '/create') {
+    fs.readdir('./data', function (err, filelist) {
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+      var body = `<form action="http://localhost:3000/precess_create" method="post">
+      <p>
+        <input type="text" name="title" placeholder="title"/>
+      </p>
+      <p>
+        <textarea name="description" placeholder="description" cols="30" rows="10"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="submit" />
+      </p>
+    </form>
+    `;
+
+      var template = templateHTML(title, list, body);
+
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
     response.writeHead(404);
     response.end('Not found');
