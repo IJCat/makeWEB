2c60d5ce2259ee79fd87e604d746c12fd3d71860
juncat <blue3197@gmail.com>
2022-02-02 23:58:54 +0900

    20220202 express middleware: make my middleware

--- nodejs-express/main.js
+++ nodejs-express/main.js
@@ -17,57 +17,60 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // compress all responses
 app.use(compression());
 
-app.get('/', (req, res) => {
-  fs.readdir('./data', (error, filelist) => {
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(filelist);
-    var html = template.HTML(
-      title,
-      list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    res.send(html);
+// My milldeware
+app.get('*', function (req, res, next) {
+  fs.readdir('./data', function (error, filelist) {
+    req.list = filelist;
+    next();
   });
 });
 
+app.get('/', (req, res) => {
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(req.list);
+  var html = template.HTML(
+    title,
+    list,
+    `<h2>${title}</h2>${description}`,
+    `<a href="/create">create</a>`
+  );
+  res.send(html);
+});
+
 app.get('/page/:pageId', (req, res) => {
-  fs.readdir('./data', (error, filelist) => {
-    var filteredId = path.parse(req.params.pageId).base;
+  var filteredId = path.parse(req.params.pageId).base;
 
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-      var title = req.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1'],
-      });
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+    var title = req.params.pageId;
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ['h1'],
+    });
 
-      var list = template.list(filelist);
-      var html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
+    var list = template.list(req.list);
+    var html = template.HTML(
+      sanitizedTitle,
+      list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
           <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
-      );
-      res.send(html);
-    });
+    );
+    res.send(html);
   });
 });
 
 app.get('/create', (req, res) => {
-  fs.readdir('./data', (error, filelist) => {
-    var title = 'create';
-    var list = template.list(filelist);
-    var html = template.HTML(
-      title,
-      list,
-      `
+  var title = 'create';
+  var list = template.list(req.list);
+  var html = template.HTML(
+    title,
+    list,
+    `
       <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -78,10 +81,9 @@ app.get('/create', (req, res) => {
         </p>
       </form>
       `,
-      ''
-    );
-    res.send(html);
-  });
+    ''
+  );
+  res.send(html);
 });
 
 app.post('/create_process', (req, res) => {
@@ -112,16 +114,15 @@ app.post('/create_process', (req, res) => {
 });
 
 app.get('/update/:pageId', (req, res) => {
-  fs.readdir('./data', (error, filelist) => {
-    var filteredId = path.parse(req.params.pageId).base;
+  var filteredId = path.parse(req.params.pageId).base;
 
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-      var title = req.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(
-        title,
-        list,
-        `
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+    var title = req.params.pageId;
+    var list = template.list(req.list);
+    var html = template.HTML(
+      title,
+      list,
+      `
         <form action="/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -133,11 +134,10 @@ app.get('/update/:pageId', (req, res) => {
           </p>
         </form>
         `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
 
-      res.send(html);
-    });
+    res.send(html);
   });
 });
 
