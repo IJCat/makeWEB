c86f075508b7df61a27e462146834ca7a7946519
IvoryCat <blue3197@gmail.com>
2022-02-09 20:45:09 +0900

    20220209 Express - Route

--- nodejs-express/main.js
+++ nodejs-express/main.js
@@ -1,15 +1,15 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const qs = require('querystring');
-const template = require('./lib/template.js');
-const path = require('path');
-const sanitizeHtml = require('sanitize-html');
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var qs = require('querystring');
+var bodyParser = require('body-parser');
+var sanitizeHtml = require('sanitize-html');
+var compression = require('compression');
+var template = require('./lib/template.js');
+var topicRouter = require('./routes/topic');
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const compression = require('compression');
+var { param } = require('express/lib/request');
 
 app.use(express.static('public'));
 
@@ -27,6 +27,8 @@ app.get('*', function (req, res, next) {
   });
 });
 
+app.use('/topic', topicRouter);
+
 app.get('/', (req, res) => {
   var title = 'Welcome';
   var description = 'Hello, Node.js';
@@ -37,137 +39,9 @@ app.get('/', (req, res) => {
     `<h2>${title}</h2>${description}
     <img src="/images/hello.jpg" style="width:400px; display:block; margin-top:10px;">
     `,
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   res.send(html);
-});
-
-app.get('/page/:pageId', (req, res, next) => {
-  var filteredId = path.parse(req.params.pageId).base;
-
-  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-    if (err) {
-      next(err);
-    } else {
-      var title = req.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1'],
-      });
-
-      var list = template.list(req.list);
-      var html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-      );
-      res.send(html);
-    }
-  });
-});
-
-app.get('/create', (req, res) => {
-  var title = 'create';
-  var list = template.list(req.list);
-  var html = template.HTML(
-    title,
-    list,
-    `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-    ''
-  );
-  res.send(html);
-});
-
-app.post('/create_process', (req, res) => {
-  /* var body = '';
-
-  req.on('data', data => (body += data));
-
-  req.on('end', () => {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-
-    fs.writeFile(`data/${title}`, description, 'utf8', err => {
-      // res.writeHead(302, { Location: `/page/${title}` });
-      // res.end();
-      res.redirect(`/page/${title}`);
-    });
-  }); 
-  */
-
-  var post = req.body;
-  var title = post.title;
-  var description = post.description;
-
-  fs.writeFile(`data/${title}`, description, 'utf8', err =>
-    res.redirect(`/page/${title}`)
-  );
-});
-
-app.get('/update/:pageId', (req, res) => {
-  var filteredId = path.parse(req.params.pageId).base;
-
-  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-    var title = req.params.pageId;
-    var list = template.list(req.list);
-    var html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-    );
-
-    res.send(html);
-  });
-});
-
-app.post('/update_process', (req, res) => {
-  var post = req.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-
-  fs.rename(`data/${id}`, `data/${title}`, error => {
-    fs.writeFile(`data/${title}`, description, 'utf8', err =>
-      res.redirect(`/page/${title}`)
-    );
-  });
-});
-
-app.post('/delete_process', (req, res) => {
-  var post = req.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-
-  fs.unlink(`data/${filteredId}`, error => res.redirect('/'));
 });
 
 app.use((req, res, next) => {
