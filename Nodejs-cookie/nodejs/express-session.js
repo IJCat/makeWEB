3b9d696f4826478f05eefa57f8b6eb77edf568ad
JuneCat <blue3197@gmail.com>
2022-03-12 11:48:38 +0900

    express session: session stroe

--- Nodejs-cookie/nodejs/express-session.js
+++ Nodejs-cookie/nodejs/express-session.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var parseurl = require('parseurl');
 var session = require('express-session');
-var File;
+var FileStore = require('session-file-store')(session);
+
 var app = express();
 
 app.use(
@@ -9,6 +10,7 @@ app.use(
     secret: 'asdfasdfljhi2lkj2@@asdf', //버전관리를 한다면 다른걸로 바꿔서 올리거나 변수처리 해야한다.
     resave: false, // 그냥  false로 하면 됨. resave가 false이면 세션 데이터 값이 바뀌기 전까지는 세션 저장소의 값을 저장하지 않는다.
     saveUninitialized: true, // 그냥  true로 하면 됨. 세션이 필요하기 전까지는 세션을 구동시키지 않는다.
+    store: new FileStore(),
   })
 );
 
