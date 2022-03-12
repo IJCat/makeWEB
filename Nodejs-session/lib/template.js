1d724db0f8b1e1dc34ea535271a0c40324d9d0cc
JuneCat <blue3197@gmail.com>
2022-03-12 12:44:40 +0900

    express session: Apply the Auth status on UI.

--- Nodejs-session/lib/template.js
+++ Nodejs-session/lib/template.js
@@ -1,14 +1,20 @@
 module.exports = {
-  HTML: function (title, list, body, control) {
+  HTML: function (
+    title,
+    list,
+    body,
+    control,
+    authStatusUI = `<a href="/auth/login">login</a>`
+  ) {
     return `
     <!doctype html>
     <html>
     <head>
+      ${authStatusUI}
       <title>Web - ${title}</title>
       <meta charset="utf-8">
     </head>
     <body>
-      <a href="/auth/login">login</a>  
       <h1><a href="/">WEB</a></h1>
       ${list}
       ${control}
--- Nodejs-session/routes/index.js
+++ Nodejs-session/routes/index.js
@@ -2,7 +2,25 @@ var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js');
 
+function authIsOwner(req, res) {
+  if (req.session.is_logined) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function authStatusUI(req, res) {
+  var authStatusUI = `<a href="/auth/login">login</a>`;
+  if (authIsOwner(req, res)) {
+    authStatusUI = `${req.session.nickname} | <a href="/auth/logout">logout</a>`;
+  }
+  return authStatusUI;
+}
+
 router.get('/', (req, res) => {
+  console.log(req.session);
+
   var title = 'Welcome';
   var description = 'Hello, Node.js';
   var list = template.list(req.list);
@@ -12,7 +30,8 @@ router.get('/', (req, res) => {
     `<h2>${title}</h2>${description}
     <img src="/images/hello.jpg" style="width:400px; display:block; margin-top:10px;">
     `,
-    `<a href="/topic/create">create</a>`
+    `<a href="/topic/create">create</a>`,
+    authStatusUI(req, res)
   );
   res.send(html);
 });
