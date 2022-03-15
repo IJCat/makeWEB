be1e213f6287e35107b3edabf24efa48dc060c85
JuneCat <blue3197@gmail.com>
2022-03-15 10:00:10 +0900

    lowdb push

--- lowdb/main.js
+++ lowdb/main.js
@@ -17,8 +17,25 @@ await db.read();
 // db.data = db.data || { posts: [] } // Node < v15.x
 db.data ||= { topic: [], author: [] }; // Node >= 15.x
 
-// // Create and query items using plain JS
-// db.data.posts.push('hello world');
+// Create and query items using plain JS
+// db.data.author.push({
+//   id: 1,
+//   name: 'egoing',
+//   profile: 'developer',
+// });
+// db.data.topic.push({
+//   id: 1,
+//   title: 'lowdb',
+//   description: 'lowdb is...',
+//   author: 1,
+// });
+// db.data.topic.push({
+//   id: 2,
+//   title: 'mysql',
+//   description: 'mysql is...',
+//   author: 1,
+// });
+
 // const firstPost = db.data.posts[0];
 
 // // Alternatively, you can also use this syntax if you prefer
@@ -26,4 +43,7 @@ db.data ||= { topic: [], author: [] }; // Node >= 15.x
 // posts.push('hello world');
 
 // // Finally write db.data content to file
-await db.write();
+// await db.write();
+
+// Read data
+const topics = db.chain.get('topic').find({ id: 1 }).value();
